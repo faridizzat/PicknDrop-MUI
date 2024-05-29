d534275ded19b538ae6339025ede5099fda53986
i.faridizzat <i.faridizzat@gmail.com>
2024-05-29 07:05:39 +0000

    feaet: add ActionButton

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import InputChildName from "../components/InputChildName"
 import Cards from "../components/Cards"
 import DateToday from "../components/DateToday"
 import ChildStatus from "../components/ChildStatus"
+import ActionButton from "../components/ActionButton"
 
 
 const HomePage = () => {
@@ -27,6 +28,10 @@ const HomePage = () => {
         {/* Avatar */}
         <Box p={2} m={2} display={"flex"} justifyContent={"center"}>
         <Cards />
+        </Box>
+
+        <Box p={2} m={2} display={"flex"} justifyContent={"center"}>
+        <ActionButton />
         </Box>
 
         <Box p={2} m={2} display={"flex"} justifyContent={"center"}>
