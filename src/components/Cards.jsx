d96e51dab4409acb05b8136b48013e58853c1821
i.faridizzat <i.faridizzat@gmail.com>
2024-05-30 09:44:38 +0000

    modify state

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,15 +1,19 @@
-import { Avatar, Box, FormControl, Typography, Checkbox } from "@mui/material";
+import { Avatar, Box, Typography, Checkbox } from "@mui/material";
 import PropTypes from "prop-types";
 import { useState } from "react";
 
 const Cards = (props) => {
   const childList = props.childList;
 
-  // const [newChildList, setNewChildList] = useState(childList);
 
-  const handleSelectChild = (event) => {
-    console.log(event);
-  };
+
+  const handleChecked = (event) => {
+    const targetId = event.target.id;
+    const targetChecked = event.target.checked;
+
+    console.log(targetId, targetChecked);
+
+  }
 
   return (
     <Box
@@ -26,30 +30,30 @@ const Cards = (props) => {
           m={2}
           key={child.id}
         >
+          <label htmlFor={child.id}>
+            <Checkbox
+              id={child.id}
+              name={child.name}
+              imgPath={child.imgPath}
+              sx={{ display: "none" }}
+              checked={isSelected}
+              onChange={handleChecked}
+            />
 
-            <label htmlFor={child.id}>
-              <Checkbox
-                id={child.id}
-                sx={{ display: "none" }}
-                onChange={handleSelectChild}
-                checked={props.isSelected}
-              />
+            <Avatar
+              alt={child.name}
+              src={child.imgPath}
+              sx={{
+                width: 100,
+                height: 100,
+                // border: props.isSelected ? "2px solid black" : "none",
+              }}
+            />
+          </label>
 
-              <Avatar
-                alt={child.name}
-                src={child.imgPath}
-                sx={{
-                  width: 100,
-                  height: 100,
-                  border: props.isSelected ? "2px solid black" : "none",
-                }}
-                onClick={props.handleChecked}
-              />
-            </label>
-
-            <Typography variant="h5" p={2}>
-              {child.name}
-            </Typography>
+          <Typography variant="h5" p={2}>
+            {child.name}
+          </Typography>
         </Box>
       ))}
     </Box>
@@ -58,8 +62,6 @@ const Cards = (props) => {
 
 Cards.propTypes = {
   childList: PropTypes.array,
-  handleChecked: PropTypes.func,
-  isSelected: PropTypes.bool,
 };
 
 export default Cards;
