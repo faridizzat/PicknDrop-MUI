ce7c80d59c432dcb6a1a02685bc557fdc7813786
Farid Izzat <i.faridizzat@gmail.com>
2024-06-02 00:21:07 +0800

    fix: remove unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import HomePage from "./pages/Home";
 
-
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
-      {/* <Main /> */}
       <HomePage />
     </>
   );
--- src/styles/homePage.styles.js
+++
@@ -1,9 +0,0 @@
-const Styles = {
-    deleteIcon: {
-      width: 100,
-      height: 100,
-      },
-};
-  
-export default Styles;
-  
