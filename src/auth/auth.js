1a6c32ee53aa16090038c14ac7585da32f8e352e
Farid Izzat <i.faridizzat@gmail.com>
2024-06-11 23:02:56 +0800

    prep for context

--- src/auth/auth.js
+++
@@ -1,5 +0,0 @@
-const auth = () => {
-  return <div>auth</div>;
-};
-
-export default auth;
--- src/routes/AuthCheck.js
+++
@@ -1,9 +0,0 @@
-const auhtCheck = () => {
-  const token = window.localStorage.getItem("token");
-  if (token) {
-    return true;
-  } else {
-    return false;
-  }
-};
-export default auhtCheck;
