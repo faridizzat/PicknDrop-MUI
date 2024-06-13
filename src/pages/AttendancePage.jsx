06eb4130d3d5be3d3ba7158e7d53a93b29f7ef82
Farid Izzat <i.faridizzat@gmail.com>
2024-06-13 22:53:13 +0800

    add table and api integration

--- src/pages/AttendancePage.jsx
+++ src/pages/AttendancePage.jsx
@@ -1,13 +1,61 @@
 import Navbar from "../components/Navbar";
 import Container from "@mui/material/Container";
-import AttendanceTable from "../components/AttendanceTable";
+import { getAttandanceById } from "../api/attendance";
+import { useState, useEffect } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const AttendancePage = () => {
+  const [attendanceList, setAttendanceList] = useState([]);
+
+  const getAttendanceList = async () => {
+    const attendanceListData = await getAttandanceById();
+    const attendanceList = attendanceListData.data;
+    setAttendanceList(attendanceList);
+  };
+
+  useEffect(() => {
+    getAttendanceList();
+  }, []);
+
+  console.log(attendanceList);
+
   return (
     <>
       <Navbar />
       <Container maxWidth="lg">
-        <AttendanceTable />
+        <TableContainer component={Paper} sx={{ mt: 2 }}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell align="right">Date</TableCell>
+                <TableCell align="right">Pick Up time</TableCell>
+                <TableCell align="right">Drop Off time</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {attendanceList.map((list) => (
+                <TableRow
+                  key={list.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {list.child_name}
+                  </TableCell>
+                  <TableCell align="right">{list.attendance_date}</TableCell>
+                  <TableCell align="right">{list.pickup_time}</TableCell>
+                  <TableCell align="right">{list.dropoff_time}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Container>
     </>
   );
